f93b56ab10eec7601b5f9e8d79e3457c4c44e8ca
aviyu <aviyu@Aviyu.lan>
2018-02-28 18:49:08 +0800

    0.0.2

---
+++ models/asdf.js
@@ -0,0 +1,21 @@
+export default {
+    namespace: 'asdf',
+    state: {
+        model_name:"asdf"
+    },
+    subscriptions: {
+        setup({ dispatch, history }) {  // eslint-disable-line
+        },
+    },
+    effects: {
+        *fetch({ payload }, { call, put }) {  // eslint-disable-line
+            yield put({ type: 'save' });
+        },
+    },
+    reducers: {
+        save(state, action) {
+            return { ...state, ...action.payload };
+        },
+    },
+};
+
